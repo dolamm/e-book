a24af8d0ae322cf0b8e15ae663abc8f6c9f1be08
yamisora-sys <20520930@ms.uit.edu.vn>
2022-12-11 10:16:42 +0700

    before pull

--- src/components/Detail-book/DetailBook.js
+++ src/components/Detail-book/DetailBook.js
@@ -43,7 +43,6 @@ export function DetailBook() {
   };
 
   const [book, setBook] = useState([]);
-
   useEffect(() => {
     getInfo().then((data) => {
       setBook(data);
@@ -68,7 +67,11 @@ export function DetailBook() {
           <h1 className="Book-title">{book.title}</h1>
           <div className="Category">
             <b>Category: </b>
-            tên Category
+            {
+              book.category.map((item) => {
+                return <span className='category-item'> {item} </span>
+              })
+            }
           </div>
           <div className="Price">
             <b>Price: </b>
